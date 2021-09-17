d25c88ad5f3ef5ae95d92372b9b963f871111f57
lenovo <lenovo@IDEAPAD>
2021-09-17 23:12:35 +0200

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> { 
+                new Product{ProductID=1,CategoryID=1,ProductName="T-Shirt",UnitPrice=5,UnitsInStock=10},
+                new Product{ProductID=2,CategoryID=1,ProductName="Shirt",UnitPrice=8,UnitsInStock=10},
+                new Product{ProductID=3,CategoryID=2,ProductName="Jean",UnitPrice=20,UnitsInStock=10},
+                new Product{ProductID=4,CategoryID=3,ProductName="Jacket",UnitPrice=30,UnitsInStock=10},
+                new Product{ProductID=5,CategoryID=2,ProductName="Skirt",UnitPrice=15,UnitsInStock=10}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            _products.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryID == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductID == product.ProductID);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryID = product.CategoryID;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
