c5ceb051ccb392677135b29a9f39f85a1d0d87a8
Erman Karataş <ermankaratash@yahoo.com>
2021-09-23 22:59:48 +0200

    A base layer can be used in all projects is created.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,12 +11,35 @@ namespace ConsoleUI
 {
     class Program
     {
-        static void Main(string[] args) 
+        static void Main(string[] args)
+        {
+            //ProductTest();
+            //CategoryTest();
+            ProductTestJoin();
+        }
+
+        private static void ProductTestJoin()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+        }
+        private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
             foreach (var product in productManager.GetAll())
             {
                 Console.WriteLine(product.ProductName);
+            }
+        }
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
             }
         }
     }
